1f6b9114d08e6d6809c77547919b62e2d436d433
denisk13005 <kirmanndenis@gmail.com>
2022-06-30 08:48:23 +0200

    build and export for test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home({posts}) {
   // méthode avec react qui utilise un fetch normal
